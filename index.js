5baaf20a3dc4fed78ff97204caf0c061e7d24400
kevin@kevin <kevin1kipkurui@gmail.com>
2022-06-19 14:43:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+async function submitData(name, email) {
+    try {
+        const response = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+            }),
+        });
+        const object = await response.json();
+        document.body.innerHTML = object["id"];
+    } catch (error) {
+        document.body.innerHTML = error.message;
+    }
+  }
+  
